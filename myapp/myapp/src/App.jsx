5bc7db13327f41fdb21937169af8d5a39296b81d
alokksharmaa <aloksharma1097@gmail.com>
2025-11-16 21:54:21 +0530

    sync

--- myapp/myapp/src/App.jsx
+++ myapp/myapp/src/App.jsx
@@ -1,30 +1,17 @@
 import "./App.css";
-import StudentDetail from "./Components/StudentDetail";
+import Student_Detail from "./Components/Student_Detail";
+
+
 
 function App() {
-  const obj1 = {
-    name: "Alok",
-    Regnumber: 1234212,
-    Address: "Lpu",
-    Number: 788544582,
-  };
-
-  const obj2 = {
-    name: "Alok",
-    Regnumber: 12343425,
-    Address: "Lpu",
-    Number: 8556655952,
-  };
-
+ 
 
   return (
 
       <>
-      <StudentDetail temp={obj1}/>
+      
 
-            <p>-----</p>
-
-      <StudentDetail temp={obj2}/>
+      <Student_Detail/>
 </>
   );
 }
---
+++ myapp/myapp/src/Components/Student_Detail.jsx
@@ -0,0 +1,11 @@
+function App() {
+  function Name() {
+    console.log("Alok");
+  }
+  return (
+    <>
+      <p>Name is printed {Name}</p>
+    </>
+  );
+}
+export default App;
