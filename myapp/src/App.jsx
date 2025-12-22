a5f854f4e9b240e16760c2752ac81e805e69a1b2
Alok Kumar Sharma <aloksharma1097@gmail.com>
2025-12-22 17:18:01 +0530

    new files added

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,13 +1,23 @@
-import "./App.css";
 import React from "react";
-import Counter from "./Hooks/reducer";
-
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./features/counter/counterSlice";
 function App() {
+  // Read value from Redux store
+  const count = useSelector((state) => state.counter.value);
+  // Dispatch actions
+  const dispatch = useDispatch();
   return (
-    <>
-      <Counter />
-    </>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Redux Counter Example1</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button
+        onClick={() => dispatch(decrement())}
+        style={{ marginLeft: "10px" }}
+      >
+        Decrement
+      </button>
+    </div>
   );
 }
-
 export default App;
--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./Redux/App";
+import Abc from "./Redux/Abc";
+import Xyz from "./Redux/xyz";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Abc />
+      <Xyz />
+    </Provider>
   </React.StrictMode>
 );
